a0e8baf5bcaba26e35dcfc6c0d99cf92b8a64adc
Laslo Hunhold <hunhold@uni-koeln.de>
2024-07-31 09:30:08 +0200

    Introduce the barred logarithmic value as an internal representation
    Inspired by the Yonemoto approach to avoid two's complementing the
    mantissa by using the internal representation
    
    	([1 - 3 * S]  + f)
    
    instead of
    
    	(-1)^S * (1 + f),
    
    we use the internal representation for the logarithmic value
    
    	l̅ := c + m,
    
    and call it 'barred logarithmic value', instead of the logarithmic value
    
    	l = (-1)^S * (c + m)
    
    This does not affect subsequent arithmetic operations too much, as they
    have to handle both positive and negative logarithmic values gracefully
    anyway.
    
    Signed-off-by: Laslo Hunhold <hunhold@uni-koeln.de>

--- rtl/decoder/decoder_logarithmic.vhd
+++ rtl/decoder/decoder_logarithmic.vhd
@@ -8,20 +8,18 @@ entity decoder_logarithmic is
 		n : natural range 2 to natural'high := 16
 	);
 	port (
-		takum             : in    std_ulogic_vector(n - 1 downto 0);
-		sign              : out   std_ulogic;
-		logarithmic_value : out   std_ulogic_vector(n + 3 downto 0); -- 9 bits integer, n-5 bits fractional
-		precision         : out   natural range 0 to n - 5;
-		is_zero           : out   std_ulogic;
-		is_nar            : out   std_ulogic
+		takum                    : in    std_ulogic_vector(n - 1 downto 0);
+		sign                     : out   std_ulogic;
+		barred_logarithmic_value : out   std_ulogic_vector(n + 3 downto 0); -- 9 bits integer, n-5 bits fractional
+		precision                : out   natural range 0 to n - 5;
+		is_zero                  : out   std_ulogic;
+		is_nar                   : out   std_ulogic
 	);
 end entity decoder_logarithmic;
 
 architecture rtl of decoder_logarithmic is
-	signal sign_internal                : std_ulogic;
-	signal characteristic               : integer range -255 to 254;
-	signal mantissa_bits                : std_ulogic_vector(n - 6 downto 0);
-	signal characteristic_mantissa_bits : std_ulogic_vector(n + 3 downto 0);
+	signal characteristic : integer range -255 to 254;
+	signal mantissa_bits  : std_ulogic_vector(n - 6 downto 0);
 begin
 
 	common_decoder : entity work.common_decoder(rtl)
@@ -30,7 +28,7 @@ begin
 		)
 		port map (
 			takum          => takum,
-			sign           => sign_internal,
+			sign           => sign,
 			characteristic => characteristic,
 			mantissa_bits  => mantissa_bits,
 			precision      => precision,
@@ -38,12 +36,7 @@ begin
 			is_nar         => is_nar
 		);
 
-	-- output sign
-	sign <= sign_internal;
-
-	-- negate the concatenated characteristic and mantissa bits depending on the sign to obtain the
-	-- logarithmic value
-	characteristic_mantissa_bits <= std_ulogic_vector(to_signed(characteristic, 9)) & mantissa_bits;
-	logarithmic_value            <= characteristic_mantissa_bits when sign_internal = '0' else
-	                                std_ulogic_vector(to_signed(-to_integer(signed(characteristic_mantissa_bits)), n + 4));
+	-- the barred logarithmic value is just c + m, i.e. the concatenation
+	-- of the characteristic signed integer bits and the mantissa bits
+	barred_logarithmic_value <= std_ulogic_vector(to_signed(characteristic, 9)) & mantissa_bits;
 end architecture rtl;
--- rtl/encoder/encoder_logarithmic.vhd
+++ rtl/encoder/encoder_logarithmic.vhd
@@ -8,32 +8,27 @@ entity encoder_logarithmic is
 		n : natural range 2 to natural'high := 64
 	);
 	port (
-		sign              : in    std_ulogic;
-		logarithmic_value : in    std_ulogic_vector(n + 3 downto 0); -- 9 bits integer, n-5 bits fractional
-		is_zero           : in    std_ulogic;
-		is_nar            : in    std_ulogic;
-		takum             : out   std_ulogic_vector(n - 1 downto 0)
+		sign                     : in    std_ulogic;
+		barred_logarithmic_value : in    std_ulogic_vector(n + 3 downto 0); -- 9 bits integer, n-5 bits fractional
+		is_zero                  : in    std_ulogic;
+		is_nar                   : in    std_ulogic;
+		takum                    : out   std_ulogic_vector(n - 1 downto 0)
 	);
 end entity encoder_logarithmic;
 
 architecture rtl of encoder_logarithmic is
-	-- signal characteristic                 : integer range -255 to 254;
-	-- signal mantissa_bits                  : std_ulogic_vector(n - 6 downto 0);
-	signal characteristic_mantissa_bits : std_ulogic_vector(n + 3 downto 0);
 begin
-	-- negate the logarithmic value depending on the sign to obtain the
-	-- characteristic and mantissa bits
-	characteristic_mantissa_bits <= logarithmic_value when sign = '0' else
-	                                std_ulogic_vector(to_signed(-to_integer(signed(logarithmic_value)), n + 4));
 
+	-- the barred logarithmic value is just c + m, i.e. the concatenation
+	-- of the characteristic signed integer bits and the mantissa bits.
 	common_encoder : entity work.common_encoder(rtl)
 		generic map (
 			n => n
 		)
 		port map (
 			sign           => sign,
-			characteristic => to_integer(signed(characteristic_mantissa_bits(n + 3 downto n - 5))),
-			mantissa_bits  => characteristic_mantissa_bits(n - 6 downto 0),
+			characteristic => to_integer(signed(barred_logarithmic_value(n + 3 downto n - 5))),
+			mantissa_bits  => barred_logarithmic_value(n - 6 downto 0),
 			is_zero        => is_zero,
 			is_nar         => is_nar,
 			takum          => takum
